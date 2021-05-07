58e178b76701134e3e96a8b2ef8f6f000457650e
G_Kumar <grshdatsts@gmail.com>
2021-05-07 23:37:39 +0530

    fix1

--- app/src/main/java/com/vinod/androidcicd/MainActivity.kt
+++ app/src/main/java/com/vinod/androidcicd/MainActivity.kt
@@ -2,11 +2,17 @@ package com.vinod.androidcicd
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.vinod.androidcicd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tv.setText("fix1-1")
+
 
     }
 }
