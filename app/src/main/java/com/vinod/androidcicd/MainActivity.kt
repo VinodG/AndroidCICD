6712d20a330d2ef0dcb953cb1f2405c4b5226358
G_Kumar <grshdatsts@gmail.com>
2021-05-10 17:56:15 +0530

    Firebase app distribution integration is done

--- app/src/main/java/com/vinod/androidcicd/MainActivity.kt
+++ app/src/main/java/com/vinod/androidcicd/MainActivity.kt
@@ -11,8 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.tv.setText("release ")
+        binding.tv.setText("release 2 ")
         println("fix2")
+
 
 
     }
